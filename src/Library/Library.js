bd52173d7b3acf634fc40d67647088d7dc894969
heloisa cativo <heloisacativo@gmail.com>
2024-10-07 22:31:44 -0300

    library just to add

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Library/Library.css";
 import bookIcon from "../assets/book-icon.png"
 import plusIcon from "../assets/plus-icon.png"
-import "./Media-query.css";
-const Library = () => {
+import { ModalAdd } from "../components/ModalAdd/ModalAdd";
+const Library = ( { addSong }) => {
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+    const isOpenModalTrue = () => {
+        setIsOpenModal(true)
+    }
+
+    const isCloseModalFalse = () => {
+        setIsOpenModal(false)
+    }
+    
     return (
         <div className="library">
-        <div className="library__content">
-            <div className="library__button">
+        <div className="libraryContent">
+            <div className="libraryButton">
                 <img src={bookIcon} alt="" />
-                <span>Sua biblioteca</span>
+                <span>Adicione</span>
             </div>
-            <div className="plus_icon">
+            <div className="plusIcon" onClick={isOpenModalTrue}>
             <img src={plusIcon} alt="" />
             </div>
-        </div>
+            {isOpenModal && (
+                <ModalAdd onClose={isCloseModalFalse} addSong={addSong}/>
+            )
 
-        <section className="section-playlist">
-            <div className="section-playlist__content">
-                <span className="text title">Crie sua primeira playlist</span>
-                <button className="section-playlist__button">
-                    <span>Criar playlist</span>
-                </button>
-            </div>
-        </section>
+            }
+        </div>
     </div>
     )
 }
