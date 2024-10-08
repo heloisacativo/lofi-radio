aa36ac40ffad3dacbd7305eb12f91f5e4fdac97b
heloisa cativo <heloisacativo@gmail.com>
2024-10-07 22:42:26 -0300

    add reset css and remove header from main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Footer from './Footer/Footer';
-import Header from './Header/Header';
 import Library from './Library/Library';
 import Play from './Play/Play';
 import Playlist from './Playlist/Playlist';
-import ArtistSearch from './Search/Search';
 
 function App() {
   return (
     <>
-    <Header />
-    <ArtistSearch />
     <Playlist />
     <Footer />
     <Play />
